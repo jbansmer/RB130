2ecf51636d47e8084e2c7993474045ecbe8bf8a2
Jacob Bansmer <jbansmer@gmail.com>
2024-09-09 12:05:20 -0700

    question 3 further exploration answered

--- Easy1/find_missing_numbers.rb
+++ Easy1/find_missing_numbers.rb
@@ -12,3 +12,27 @@ p missing([-3, -2, 1, 5]) == [-1, 0, 2, 3, 4]
 p missing([1, 2, 3, 4]) == []
 p missing([1, 5]) == [2, 3, 4]
 p missing([6]) == []
+
+# Further Exploration:
+# Solve the problem without using a method that requires a block:
+
+def missing(array)
+  all_possible_numbers = (array[0]..array[-1]).to_a
+  missed_numbers = []
+
+  counter = 0
+  loop do
+    if !array.include? all_possible_numbers[counter]
+      missed_numbers << all_possible_numbers[counter]
+    end
+    counter += 1
+    break if counter == all_possible_numbers.size
+  end
+
+  missed_numbers
+end
+
+p missing([-3, -2, 1, 5]) == [-1, 0, 2, 3, 4]
+p missing([1, 2, 3, 4]) == []
+p missing([1, 5]) == [2, 3, 4]
+p missing([6]) == []
