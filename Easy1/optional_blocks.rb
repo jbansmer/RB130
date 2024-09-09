8f2ef5f9607c7bd192b57ff793b481e41acde13a
Jacob Bansmer <jbansmer@gmail.com>
2024-09-09 11:51:20 -0700

    question 2 further exploration answered

--- Easy1/optional_blocks.rb
+++ Easy1/optional_blocks.rb
@@ -11,3 +11,20 @@ end
 p compute { 5 + 3 } == 8
 p compute { 'a' + 'b' } == 'ab'
 p compute == 'Does not compute.'
+
+
+# Further Exploration:
+# Modify the compute method so it takes a single argument and yields that argument to the block. Provide at least 3 examples of calling this new version of compute, including a no-block call.
+
+def compute(arg, &block)
+  if block_given?
+    block.call(arg)
+  else
+    'Does not compute.'
+  end
+end
+
+p compute(5) { |n| n + 4 } == 9
+p compute([1, 2, 3], &:sum ) == 6
+p compute('string', &:upcase) == 'STRING'
+p compute(4) == 'Does not compute.'
