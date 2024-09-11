6a58a7335fa481ade098ed900b5559320664e868
Jacob Bansmer <jbansmer@gmail.com>
2024-09-10 18:18:51 -0700

    question 10 answered

---
+++ Easy1/count_items.rb
@@ -0,0 +1,32 @@
+# Define a method that returns a count of number of times the passed in block returns true:
+
+def count(collection)
+  true_count = 0
+  collection.each { |ele| true_count += 1 if yield(ele) }
+  true_count
+end
+
+p count([1,2,3,4,5]) { |value| value.odd? } == 3
+p count([1,2,3,4,5]) { |value| value % 3 == 1 } == 2
+p count([1,2,3,4,5]) { |value| true } == 5
+p count([1,2,3,4,5]) { |value| false } == 0
+p count([]) { |value| value.even? } == 0
+p count(%w(Four score and seven)) { |value| value.size == 5 } == 2
+
+# Further Exploration:
+# Write a method definition for #count that does not include each, loop, while, or until:
+
+def count(collection)
+  true_count = 0
+  for ele in collection
+    true_count += 1 if yield(ele)
+  end
+  true_count
+end
+
+p count([1,2,3,4,5]) { |value| value.odd? } == 3
+p count([1,2,3,4,5]) { |value| value % 3 == 1 } == 2
+p count([1,2,3,4,5]) { |value| true } == 5
+p count([1,2,3,4,5]) { |value| false } == 0
+p count([]) { |value| value.even? } == 0
+p count(%w(Four score and seven)) { |value| value.size == 5 } == 2
