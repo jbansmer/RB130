8ccf67890e54f9273003b89a1eeda5ab49e2ada3
Jacob Bansmer <jbansmer@gmail.com>
2024-09-12 13:46:12 -0700

    question 7 answered

---
+++ Easy2/each_with_object.rb
@@ -0,0 +1,26 @@
+# Define an #each_with_object method that takes two arguments: a collection and a collection object. Both objects are passed to a given block where the collection object may be updated. This colleciton object should be the return value.
+
+def each_with_object(collection, object)
+  collection.each { |ele| yield(ele, object) }
+  object
+end
+
+result = each_with_object([1, 3, 5], []) do |value, list|
+  list << value**2
+end
+p result == [1, 9, 25]
+
+result = each_with_object([1, 3, 5], []) do |value, list|
+  list << (1..value).to_a
+end
+p result == [[1], [1, 2, 3], [1, 2, 3, 4, 5]]
+
+result = each_with_object([1, 3, 5], {}) do |value, hash|
+  hash[value] = value**2
+end
+p result == { 1 => 1, 3 => 9, 5 => 25 }
+
+result = each_with_object([], {}) do |value, hash|
+  hash[value] = value * 2
+end
+p result == {}
