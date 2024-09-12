caa52471bbd87a1aa5f36d81b8e93e1333b9b047
Jacob Bansmer <jbansmer@gmail.com>
2024-09-12 13:08:18 -0700

    question 4 answered

---
+++ Easy2/count.rb
@@ -0,0 +1,13 @@
+# Define a #count method that returns the number of elements that return a truthy block value in an arbitrarily long list of arguments:
+
+def count(*args)
+  truthy_values = args.select { |value| yield value }
+  truthy_values.length
+end
+
+p count(1, 3, 6) { |value| value.odd? } == 2
+p count(1, 3, 6) { |value| value.even? } == 1
+p count(1, 3, 6) { |value| value > 6 } == 0
+p count(1, 3, 6) { |value| true } == 3
+p count() { |value| true } == 0
+p count(1, 3, 6) { |value| value - 6 } == 3
