c574876da4f99140544022e1f52d4762d75e77e2
Jacob Bansmer <jbansmer@gmail.com>
2024-09-17 13:29:16 -0700

    testing file created and set up

---
+++ MediumTesting/cash_register_test.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require_relative 'cash_register'
+require_relative 'transaction'
+require 'minitest/reporters'
+Minitest::Reporters.use!
+
+class CashRegisterTest < Minintest::Test
+
+end
