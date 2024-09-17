001073c3db67f86728da98b8faa8d77fac86b6b2
Jacob Bansmer <jbansmer@gmail.com>
2024-09-17 13:22:59 -0700

    question 8 further exploration answered

--- Medium1/bubble_sort.rb
+++ Medium1/bubble_sort.rb
@@ -40,3 +40,27 @@ p array == %w(Kim Pete Tyler alice bonnie rachel sue)
 array = %w(sue Pete alice Tyler rachel Kim bonnie)
 bubble_sort!(array) { |first, second| first.downcase <= second.downcase }
 p array == %w(alice bonnie Kim Pete rachel sue Tyler)
+
+# further exploration:
+# Modify your solution so it only passes one element to the block at a time; the block should perform some sort of transformation to the argument, and #bubble_sort! itself should just use <= to compare two values.
+
+def bubble_sort!(array)
+  loop do
+    swapped = false
+    1.upto(array.size - 1) do |index|
+      if block_given?
+        next if yield(array[index - 1]) <= yield(array[index])
+      else
+        next if array[index - 1] <= array[index]
+      end
+      array[index - 1], array[index] = array[index], array[index - 1]
+      swapped = true
+    end
+
+    break unless swapped
+  end
+end
+
+array = %w(sue Pete alice Tyler rachel Kim bonnie)
+bubble_sort!(array) { |value| value.downcase }
+p array == %w(alice bonnie Kim Pete rachel sue Tyler)
