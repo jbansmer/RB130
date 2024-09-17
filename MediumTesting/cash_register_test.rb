37ac07768363dfed72b65955711d80b93726e60b
Jacob Bansmer <jbansmer@gmail.com>
2024-09-17 13:37:57 -0700

    test for accept_money method added

--- MediumTesting/cash_register_test.rb
+++ MediumTesting/cash_register_test.rb
@@ -1,9 +1,12 @@
 require 'minitest/autorun'
 require_relative 'cash_register'
 require_relative 'transaction'
-require 'minitest/reporters'
-Minitest::Reporters.use!
 
-class CashRegisterTest < Minintest::Test
-
+class CashRegisterTest < Minitest::Test
+  def test_accept_money
+    cash_register = CashRegister.new(100)
+    transaction = Transaction.new(50)
+    transaction.amount_paid = 50
+    assert_equal(150, cash_register.accept_money(transaction))
+  end
 end
