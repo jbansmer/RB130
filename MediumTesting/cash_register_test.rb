d89ce04003811dcb331e61e0a23c2d1bd15cc749
Jacob Bansmer <jbansmer@gmail.com>
2024-09-18 10:33:21 -0700

    updated prompt_for_payment and associated test to eliminate output during test

--- MediumTesting/cash_register_test.rb
+++ MediumTesting/cash_register_test.rb
@@ -29,7 +29,8 @@ class CashRegisterTest < Minitest::Test
   def test_prompt_for_payment
     transaction = Transaction.new(50)
     input = StringIO.new("50\n")
-    transaction.prompt_for_payment(input: input)
+    output = StringIO.new
+    transaction.prompt_for_payment(input: input, output: output)
     assert_equal(50, transaction.amount_paid)
   end
 end
