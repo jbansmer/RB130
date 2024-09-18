27baf7573bc839f60fecdb818d35f10326e924f7
Jacob Bansmer <jbansmer@gmail.com>
2024-09-18 12:44:14 -0700

    word_count test added

--- MediumTesting/text_swap_exercise_test.rb
+++ MediumTesting/text_swap_exercise_test.rb
@@ -20,24 +20,12 @@ class TextTest < Minitest::Test
     assert_equal(expected_text, actual_text)
   end
 
+  def test_word_count
+    text = Text.new(@sample_text.read)
+    assert_equal(72, text.word_count)
+  end
+
   def teardown
     @sample_text.close
   end
 end
-
-# class Text
-#   def initialize(text)
-#     @text = text
-#   end
-
-#   def swap(letter_one, letter_two)
-#     @text.gsub(letter_one, letter_two)
-#   end
-# end
-
-# Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed vulputate ipsum.
-# Suspendisse commodo sem arcu. Donec a nisi elit. Nullam eget nisi commodo, volutpat
-# quam a, viverra mauris. Nunc viverra sed massa a condimentum. Suspendisse ornare justo
-# nulla, sit amet mollis eros sollicitudin et. Etiam maximus molestie eros, sit amet dictum
-# dolor ornare bibendum. Morbi ut massa nec lorem tincidunt elementum vitae id magna. Cras
-# et varius mauris, at pharetra mi.
